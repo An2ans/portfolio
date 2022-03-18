bad74ed1401b9a1f08fa3238bec49a02b9b62b94
An2ans <antoniojesus_92@hotmail.com>
2022-03-18 14:37:08 +0000

    new fix

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,30 +1,16 @@
 import styles from "../../styles/projectpage.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import { projectsTable } from "../../airtable/airtable";
+import {
+  projectsTable,
+  fetchProjectsFromAirtable,
+} from "../../airtable/airtable";
 import { useEffect, useState } from "react";
 
 //In order to get dynamic paths, we need getStaticPaths, which return an array with all paths static generated (paths) and a bool fallback
 //To get the paths, I have fetched all projects and mapped using same params as [project], Ive pass it to lowercase (to make it not case sens) and toString to transform spaces
 export const getStaticPaths = async () => {
-  const fetchProjects = async () => {
-    try {
-      let projects = [];
-      await projectsTable.select().eachPage((records, fetchNextPage) => {
-        records.forEach((record) => {
-          projects.push(record.fields);
-        });
-        fetchNextPage();
-      });
-      return projects;
-    } catch (e) {
-      return console.log({
-        message: "there was an error retrieving the records for paths",
-        e,
-      });
-    }
-  };
-  const projects = await fetchProjects();
+  const projects = await fetchProjectsFromAirtable();
   const paths = await projects.map((project) => {
     return {
       params: { project: project.name.toLowerCase().toString() },
@@ -68,24 +54,6 @@ export const getStaticProps = async (context) => {
 
 //The page component receives the props from getStaticProps
 const ProjectPage = ({ project }) => {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const fetchProjects = async (req, res) => {
-      try {
-        const response = await fetchProjectsFromAirtable();
-        setProjects(response);
-      } catch (e) {
-        console.log({
-          message: "there was an error while fetching projects",
-          e,
-        });
-      }
-    };
-    if (projects.length === 0) {
-      fetchProjects();
-    }
-  });
   const stack = project.stack.split(",");
   return (
     <div className={styles.main}>
